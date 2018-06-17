c20055260646694ebbcb5f0345b8b870096e7b36
Gerry Pass <rgpass@gmail.com>
2018-06-17 09:58:44 -0400

    :lipstick: Change button dimensions on orientation change

--- src/ui/Next.js
+++ src/ui/Next.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   Dimensions,
   StyleSheet,
@@ -7,8 +7,6 @@ import {
   View,
 } from 'react-native'
 import { func } from 'prop-types'
-
-const { height, width } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   container: {
@@ -19,8 +17,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#158CBA',
     borderRadius: 20,
-    height: height * 0.2,
-    width: width * 0.9,
   },
   text: {
     color: 'white',
@@ -28,13 +24,46 @@ const styles = StyleSheet.create({
   },
 })
 
-const Next = props => (
-  <View style={[props.containerStyle, styles.container]}>
-    <TouchableOpacity onPress={props.onPress} style={styles.button}>
-      <Text style={[props.textStyle, styles.text]}>Next</Text>
-    </TouchableOpacity>
-  </View>
-)
+class Next extends Component {
+  constructor(props) {
+    super(props)
+
+    const { height, width } = Dimensions.get('window')
+
+    this.state = {
+      height: height * 0.2,
+      width: width * 0.9,
+    }
+
+    this.onLayout = this.onLayout.bind(this)
+  }
+
+  onLayout() {
+    const { height, width } = Dimensions.get('window')
+
+    this.setState({
+      height: height * 0.2,
+      width: width * 0.9,
+    })
+  }
+
+  render() {
+    const { onLayout } = this
+    const { containerStyle, onPress, textStyle } = this.props
+    const { height, width } = this.state
+
+    return (
+      <View onLayout={onLayout} style={[containerStyle, styles.container]}>
+        <TouchableOpacity
+          onPress={onPress}
+          style={[styles.button, { height, width }]}
+        >
+          <Text style={[textStyle, styles.text]}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
 
 Next.propTypes = {
   onPress: func.isRequired,
